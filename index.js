b220725aca0727f8783e21bf7b915f98ba1c993e
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-10 22:24:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 theBeatlesPlay(musicians, instruments);
+
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(arr) {
+  var result= []; 
+  var i = 0; 
+  while(i < arr.length) {
+    result.push(arr[i] + '!!!')
+    i++; 
+  }
+  return result; 
+}
+
+johnLennonFacts(facts);
